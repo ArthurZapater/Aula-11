aa68ba2c4bf905b7bc7add39103dad7fae469ac4
Arthur Zapater <arthur.zapater@gmail.com>
2025-09-15 21:17:13 -0300

    Initial commit

---
+++ src/Exemplo01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exemplo01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] m = new int[3][2];
+
+        //Ler os dados da matriz
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print("Valor --> ");
+                m[i][j] = sc.nextInt();
+            }
+        }
+
+        //Imprimir os dados da matriz
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}

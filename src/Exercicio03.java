c643b4b477b57a3f098484c635265eeac4a80170
Arthur Zapater <arthur.zapater@gmail.com>
2025-09-19 17:24:46 -0300

    Subindo Exercicio 3.

--- src/Exercicio03.java
+++ src/Exercicio03.java
@@ -1,23 +1,25 @@
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Exercicio03 {
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.00");
         Random aleatorio = new Random();
-        int[][] x = new int[10][12];
-        int maior = Integer.MIN_VALUE, menor = Integer.MAX_VALUE;
-        int[] y = new int[10];
+        double[][] x = new double[10][12];
+        double maior = Integer.MIN_VALUE, menor = Integer.MAX_VALUE;
+        double[] y = new double[10];
 
 
         //Entrada de dados
         for (int i = 0; i < x.length; i++) {
-            int total = 0;
+            double total = 0;
             for (int j = 0; j < x[i].length; j++) {
-                x[i][j] = aleatorio.nextInt(10, 33);
+                x[i][j] = aleatorio.nextDouble(10, 33);
                 total += x[i][j];
-                System.out.print(x[i][j] + "ºC" + "\t");
+                System.out.print(df.format(x[i][j]) + "ºC" + "  ");
             }
             System.out.println();
-            total /= 12;
+            total /= x[i].length;
             y[i] = total;
 
             if (y[i] > maior) {
@@ -32,11 +34,11 @@ public class Exercicio03 {
 
         System.out.println("\nMédia dos anos");
         for (int i = 0; i < y.length; i++) {
-            System.out.println("Ano " + (i + 1) + " --> " + y[i] + "ºC");
+            System.out.println("Ano " + (i + 1) + " --> " + df.format(y[i]) + "ºC");
 
         }
 
-        System.out.println("\nMaior média --> " + maior);
-        System.out.println("Menor maior --> " + menor);
+        System.out.println("\nMaior média --> " + df.format(maior));
+        System.out.println("Menor maior --> " + df.format(menor));
     }
 }
